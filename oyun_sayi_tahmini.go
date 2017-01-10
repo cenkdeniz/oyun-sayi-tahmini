8cccfe1f0491a8ef1a975fcb59197ece782d3ae2
Cenk Deniz <cenk.dnz@gmail.com>
2017-01-11 01:24:54 +0300

    Oyun source code.

---
+++ oyun_sayi_tahmini.go
@@ -0,0 +1,35 @@
+package main
+
+import  "fmt"
+
+const MAX = 20
+const  MIN  = 1
+var sayi int
+var win  int
+
+func main(){
+	win= 10
+	oyun()
+}
+
+func oyun (){
+
+	fmt.Printf("%d ile %d arasında bir sayı söyle\n",MIN, MAX)
+	fmt.Scanln(&sayi)
+
+	if(sayi == win){
+		fmt.Printf("Tebrikler tuttuğum sayı %d\n",sayi)
+
+
+	}else if (sayi>win){
+		fmt.Printf("%d tuttuğum sayıdan büyük\n",sayi)
+		oyun()
+
+	} else if (sayi<win){
+		fmt.Printf("%d tuttuğum sayıdan küçük\n",sayi)
+		oyun()
+
+	}
+}
+
+
